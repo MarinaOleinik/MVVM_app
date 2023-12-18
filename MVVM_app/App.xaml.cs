110e0e723e7a3faf9abfa0a087d9bf3f9429bef3
marinaoleinik <marina.oleinik@tthk.ee>
2023-12-18 16:59:17 +0200

    Add project files.

---
+++ MVVM_app/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MVVM_app.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace MVVM_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FriendsListPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
